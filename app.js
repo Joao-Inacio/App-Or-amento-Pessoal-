e25609aed19fc89b2691041a2d9ce7ee3b269fa9
João Inácio <joaoinacio206@protonmail.com>
2021-03-06 19:17:29 -0300

    finalizando

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ class Bd {
 			if (despesa === null) {
 				continue
 			}
+			despesa.id = i
 			despesas.push(despesa)
 		}
 		return despesas
@@ -81,8 +82,10 @@ class Bd {
 		}
 		return despesasFiltradas
 	}
+	remover(id){
+		localStorage.removeItem(id)
+	}
 }
-
 let bd = new Bd()
 
 function cadastrarDespesa() {
@@ -163,6 +166,18 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
 		linha.insertCell(1).innerHTML = d.tipo
 		linha.insertCell(2).innerHTML = d.descricao
 		linha.insertCell(3).innerHTML = d.valor
+
+		let btn = document.createElement('button')
+		btn.className = 'btn btn-danger'
+		btn.innerHTML = '<i class="fa fa-times"  ></i>'
+		btn.id = `id_despesa_${d.id}`
+		btn.onclick = function(){
+			let id = this.id.replace('id_despesa_','')
+
+			bd.remover(id)
+			window.location.reload()
+		}
+		linha.insertCell(4).append(btn)
 	})
 }
 function pesquisarDespesa() {
@@ -179,7 +194,6 @@ function pesquisarDespesa() {
 
 	let listaDespesas = document.getElementById("listaDespesas")
 	this.carregaListaDespesas(despesas, true)
-
 	
 }
 
